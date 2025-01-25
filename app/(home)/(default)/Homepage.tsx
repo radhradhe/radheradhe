91b3bf77c36d00afccd1cc90a947d8f73443ab77
Ashish <90093173+ashurohilla@users.noreply.github.com>
2025-01-26 00:14:02 +0530

    ADD new site

--- app/(home)/(default)/Homepage.tsx
+++ app/(home)/(default)/Homepage.tsx
@@ -5,7 +5,7 @@
   import image1 from "../../assests/image1.jpeg";
   import image2 from "../../assests/image2.png";
   import image3 from "../..//assests/image3.jpeg";
-  import {Button} from "../../../components/ui/button"
+  import { Button } from "@/components/ui/button";
   import { Input } from "@/components/ui/input";
   import { LinkedInLogoIcon } from "@radix-ui/react-icons";
   import { InstagramLogoIcon } from "@radix-ui/react-icons";
